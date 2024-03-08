ea4ccb3634e3455021c87a1676c1c25f99209e9b
thin-nandar-hsu <thinnandarhsu99@gmail.com>
2024-03-09 00:33:31 +0630

    HW-ProjectIdea[ArtGallery]

---
+++ DotNetTrainingTest.HWArtApi/Controllers/ArtController.cs
@@ -0,0 +1,59 @@
+﻿using DotNetTrainingTest.HWArtApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DotNetTrainingTest.HWArtApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtController : ControllerBase
+    {
+        private readonly string _url = "https://burma-project-ideas.vercel.app/art-gallery";
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            HttpClient _httpClient = new HttpClient();
+            var response = await _httpClient.GetAsync(_url);
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                List<ArtDataModel> data = JsonConvert.DeserializeObject<List<ArtDataModel>>(json)!;
+                List<ArtViewModel> lst = data.Select(x => new ArtViewModel
+                {
+                    Id = x.ArtId,
+                    Name = x.ArtName,
+                    PictureUrl = $"https://burma-project-ideas.vercel.app/{x.ArtImageUrl}"
+
+                }).ToList();
+                return Ok(lst);
+
+            } else return BadRequest();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id) 
+        {
+            HttpClient _httpClient = new HttpClient();
+            var response = await _httpClient.GetAsync($"{_url}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync() ;
+                ArtByIdDataModel data = JsonConvert.DeserializeObject<ArtByIdDataModel>(json) !;
+                var item = new ArtByIdViewModel()
+                {
+                    ArtistId = data.Artist.ArtistId,
+                    ArtistName = data.Artist.ArtistName,
+                    FaceBookAccount = data.Artist.Social[0].Link,
+                    ArtList = data.Arts
+                };
+                return Ok(item);               
+            }else return BadRequest();
+            
+        }
+
+    }
+
+}
---
+++ DotNetTrainingTest.HWArtApi/Models/ArtModel.cs
@@ -0,0 +1,57 @@
+﻿namespace DotNetTrainingTest.HWArtApi.Models
+{
+    public class ArtDataModel
+    {
+        public int ArtId { get; set; }
+        public string ArtName { get; set; }
+        public string ArtDescription { get; set; }
+        public string ArtImageUrl { get; set; }
+    }
+
+    public class ArtViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string PictureUrl { get; set; }
+    }
+
+
+    public class ArtByIdDataModel
+    {
+        public Artist Artist { get; set; }
+        public Art[] Arts { get; set; }
+    }
+
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string ArtistName { get; set; }
+        public Social[] Social { get; set; }
+        public string ArtistImageUrl { get; set; }
+    }
+
+    public class Social
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class Art
+    {
+        public int ArtId { get; set; }
+        public string ArtName { get; set; }
+        public string ArtDescription { get; set; }
+        public string ArtImageUrl { get; set; }
+    }
+
+    public class ArtByIdViewModel
+    {
+        public int ArtistId { get; set; }
+        public string ArtistName { get; set; }
+        public string FaceBookAccount { get; set; }
+        public Art[] ArtList { get; set; }
+    }
+
+
+}

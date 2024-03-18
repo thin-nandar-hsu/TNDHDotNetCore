9c83fed5a14942262adfc3d03d69895c9bd13945
thin-nandar-hsu <thinnandarhsu99@gmail.com>
2024-03-18 21:14:06 +0630

    ASP.NetCoreMVC[Complete]

---
+++ DotNetTrainingTest.MvcApp/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DotNetTrainningTest.MvcApp.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetTrainningTest.MvcApp;
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "TestDb",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+        optionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString);
+    }
+
+    public DbSet<BlogModel> Blogs { get; set; }
+}
---
+++ DotNetTrainingTest.MvcApp/Models/BlogModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotNetTrainningTest.MvcApp.Models;
+
+[Table("Tbl_Blog")]
+public class BlogModel
+{
+    [Key]
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
+
+public class BlogMessageResponseModel
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+}
